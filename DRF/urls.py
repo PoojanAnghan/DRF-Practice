f6bddffb955cc72094f73e0c7bb3850c3134ed12
Poojan Anghan <anghanarpit@gmail.com>
2025-08-26 16:46:22 +0530

    Difrrent ViewSet added

--- DRF/urls.py
+++ DRF/urls.py
@@ -1,20 +1,36 @@
-# urls.py
-from django.urls import path
-# from DRFApi.views import LCStudentAPI, RUDStudentAPI
-from DRFApi.views import StudentCreateAPI, StudentDeleteAPI, StudentListAPI, StudentRetrieveAPI, StudentUpdateAPI
 from django.contrib import admin
+from django.urls import path, include
+from DRFApi.views import LCStudentAPI, RUDStudentAPI   # ---- GenericClassview + Mixins
+from DRFApi.views import StudentCreateAPI, StudentListAPI, StudentRetrieveAPI, StudentUpdateAPI # StudentDeleteAPI  ---- Concreate View
+from DRFApi.views import StudentViewSet, StudentGenericViewSet # ---- ViewSet
+from rest_framework.routers import DefaultRouter
 
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("student-crud/", LCStudentAPI.as_view(), name="student-list-create"),
-#     path("student-crud/<int:pk>/", RUDStudentAPI.as_view(), name="student-detail"),
+# -------------------  GenericClassview + Mixins -------------------
+# urlpatterns = [ 
+#         path("admin/", admin.site.urls),
+#         path("student-crud/", LCStudentAPI.as_view(), name="student-list-create"),
+#         path("student-crud/<int:pk>/", RUDStudentAPI.as_view(), name="student-detail"),
 # ]
 
-urlpatterns = [
 
-    path('students/', StudentListAPI.as_view(), name='student-list'),
-    path('students/create/', StudentCreateAPI.as_view(), name='student-create'),
-    path('students/<int:pk>/', StudentRetrieveAPI.as_view(), name='student-retrieve'),
-    path('students/<int:pk>/update/', StudentUpdateAPI.as_view(), name='student-update'),
-    path('students/<int:pk>/delete/', StudentDeleteAPI.as_view(), name='student-delete'),
+# ------------------- Concreate View -------------------
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path('students/', StudentListAPI.as_view(), name='student-list'),
+#     path('students/create/', StudentCreateAPI.as_view(), name='student-create'),
+#     path('students/<int:pk>/', StudentRetrieveAPI.as_view(), name='student-retrieve'),
+#     # path('students/<int:pk>/update/', StudentUpdateAPI.as_view(), name='student-update'),
+#     # path('students/<int:pk>/delete/', StudentDeleteAPI.as_view(), name='student-delete'),
+# ]
+
+# ------------------- ViewSet(simple) -------------------
+
+router = DefaultRouter()
+# router.register(r'students', StudentViewSet, basename='student')
+router.register(r'students', StudentGenericViewSet, basename='student')
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include(router.urls)),
 ]
